2b497f9581a44169123b992ba50c8f8761bf4a0c
YuliyaKasyanchuk <yuljakas@gmail.com>
2016-08-07 16:55:48 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
     var $messageField=$('.message-field');
     var $messageFormatted=$('.message-formatted');
     
-    $messageField.keyup(function(){
-    var str=$messageField.val().toLocaleLowerCase();
-    $messageFormatted.empty();  
-        
-        
-    $messageFormatted.text(str);
-    });
+        $messageField.keyup(function(){
+           var str=$messageField.val().toLocaleLowerCase(); 
+           var newStr=str.replace(/\s{2,}/g, " ").replace (/[\n\r]/g, ' ');  
+            
+           $messageFormatted.text(newStr);
+         });
                               
  });
+
